40c90b74c4b086be0f346dc94aeae70d6bcb1896
Chandler Puckett <chandler.puckett1@gmail.com>
2020-05-20 16:15:34 -0700

    JS

---
+++ date.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var today = newDate();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+if(dd<10)
+{
+    dd='0'+dd;
+}
+
+if(mm<10)
+{
+    mm='0'+mm;
+}
+today = mm+'-'+dd+'-'+yyyy;
+console.log(today);
+
+document.write('<h2>' + today)
+
+
---
+++ greeting.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var greeting= 'Howdy ';
+var name= 'Doggo Lovers!!';
+
+var welcomeMessage = greeting+ name+ '!';
+
+var el =document.getElementByld('greeting'); 
+el .textContent = welcomeMessage;
